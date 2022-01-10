a2e068b8b9f7ffb912b5d1b4522094997c105e26
MaxStump13 <stumpmaximilian@gmail.com>
2022-01-09 23:30:00 -0500

    Added inquirer and fs, created the question arrays

--- index.js
+++ index.js
@@ -15,3 +15,108 @@
 // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
+//manager is required  engineer and intern are optional
+
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const projectManQuestions = [
+    {
+        type: "input",
+        message: "What is the name of the Project Manager?",
+        name: "proj-man"
+    },
+    {
+        type: "input",
+        message: "What is the Project Manager's ID?",
+        name: "proj-man-id"
+    },
+    {
+        type: "input",
+        message: "What is the Project Managers email?",
+        name: "proj-man-email"
+    },
+    {
+        type: "input",
+        message: "What is the Project Manager's Office Number?",
+        name: "proj-man-office"
+    },
+];
+
+const engineerQuestions =[
+    {
+    
+        type: "input",
+        message: "What is the name of the Engineer?",
+        name: "engineer-name"
+    },
+    {
+        type: "input",
+        message: "What is the Engineer's ID?",
+        name: "engineer-man-id"
+    },
+    {
+    
+        type: "input",
+        message: "What is the email of the Engineer?",
+        name: "engineer-email"
+    },
+    {
+    
+        type: "input",
+        message: "What is the Engineer's GitHub username?",
+        name: "engineer-GitHub"
+    },
+];
+const internQuestions =[
+    {
+    
+        type: "input",
+        message: "What is the name of the Intern?",
+        name: "intern-name"
+    },
+    {
+        type: "input",
+        message: "What is the ID of the Intern?",
+        name: "intern-id"
+    },
+    {
+    
+        type: "input",
+        message: "What is the email of the Intern?",
+        name: "intern-email"
+    },
+    {
+    
+        type: "input",
+        message: "What is the Intern's GitHub username?",
+        name: "intern-GitHub"
+    },
+];
+const menu = [
+    {
+    input: "input",
+    message: "Would you like to add an engineer, intern, or finished building your team?",
+    name: "menu",
+    choice: ["Add Engineer", "Add Intern", "Finish building your team"]
+    }
+];
+
+
+
+function init(){
+inquirer
+.prompt(questions)
+.then(function(answers){
+console.log(answers);
+
+generate(answers);
+})
+}
+
+function generate(data){
+    
+}
+
+
+init();
